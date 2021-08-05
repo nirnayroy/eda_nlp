63c97ce7a1fb134c7d91d1297dbd06eb1debf938
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-08-05 20:26:53 +0530

    Update augment.py

--- code/augment.py
+++ code/augment.py
@@ -8,7 +8,6 @@ import pandas as pd
 import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("--input_df", required=True, type=str, help="input dataframe of unaugmented data")
-ap.add_argument("--output_df", required=True, type=str, help="output dataframe of augmented data")
 ap.add_argument("--num_aug", required=False, type=int, help="number of augmented sentences per original sentence")
 ap.add_argument("--alpha_sr", required=False, type=float, help="percent of words in each sentence to be replaced by synonyms")
 ap.add_argument("--alpha_ri", required=False, type=float, help="percent of words in each sentence to be inserted")
@@ -45,17 +44,20 @@ if alpha_sr == alpha_ri == alpha_rs == alpha_rd == 0:
      ap.error('At least one alpha should be greater than zero')
 
 #generate more data with standard augmentation
-def gen_eda(train_df, output_df, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num_aug=9):
-
+def gen_eda(train_df, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num_aug=9):
+    augmented_df = pd.DataFrame()
     for i in range(train_df.shape[0]):
         label = train_df[i][0]
         sentence = train_df[i][1]
         aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
-
+        augmented_df[i][0] = label
+        augmented_df[i][1] = aug_sentences
+    # concatenate
+    # save csv
     print("generated augmented sentences with eda for " + train_orig + " to " + output_file + " with num_aug=" + str(num_aug))
 
 #main function
 if __name__ == "__main__":
 
     #generate augmented sentences and output into a new file
-    gen_eda(args.input_df, args.output_df, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, alpha_rd=alpha_rd, num_aug=num_aug)
+    gen_eda(args.input_df, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, alpha_rd=alpha_rd, num_aug=num_aug)
